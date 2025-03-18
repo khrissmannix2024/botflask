35cbdeebbffde9665e063dd2e52da321d457d247
Khristopher <khriss201403@gmail.com>
2025-03-18 16:13:55 -0500

    Cambiando la ruta de config.py para que render la encuentre

--- main/config/config.py
+++ main/config/config.py
@@ -3,6 +3,12 @@ import secrets
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, '..', 'data', 'database.db')  
+
+    if os.environ.get("RENDER"):
+        DB_PATH = "/tmp/database.db"  # Ruta válida en Render
+    else:
+        DB_PATH = os.path.join(BASE_DIR, "..", "data", "database.db")
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + DB_PATH
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = secrets.token_hex(16)
