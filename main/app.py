d46f1af726c7f3c21da56b5370bde185664aa070
Khristopher <khriss201403@gmail.com>
2025-03-18 16:04:53 -0500

    Ordenando app.py para que render ejecute correctamente.

--- main/app.py
+++ main/app.py
@@ -1,29 +1,28 @@
-from flask import Flask
-from models.models import db, User
-from flask_migrate import Migrate
-from extensions.manager_login import login_manager
-from extensions.migrate import migrate
-
-import sys
 import os
+import sys
+from flask import Flask
+from flask_migrate import Migrate
+
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
+from models.models import db, User 
+from extensions.manager_login import login_manager
+from extensions.migrate import migrate
 from config import Config
+from routes import routes
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-DB_PATH = app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", "")
-
-if not os.path.exists(DB_PATH):  # Verifica si el archivo existe
-    with app.app_context():
-        db.create_all()
-
+# Inicializa las extensiones con la app
 db.init_app(app)
 migrate.init_app(app, db)
 login_manager.init_app(app)
 
-from routes import routes 
+DB_PATH = app.config["SQLALCHEMY_DATABASE_URI"].replace("sqlite:///", "")
+if not os.path.exists(DB_PATH):
+    with app.app_context():
+        db.create_all()
 
 app.register_blueprint(routes)
 
